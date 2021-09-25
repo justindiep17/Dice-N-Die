404111c229c3f3986f8193c8ef01a967c74c0bc2
OnlyDill <justindiep17@gmail.com>
2021-09-25 03:11:56 -0400

    First committ

---
+++ src/BtnsBarSmall.js
@@ -0,0 +1,19 @@
+import Column from "./Column";
+
+function BtnsBarSmall() {
+  const RED = "#D32F2F";
+  const GREEN = "#689F38";
+  const AMBER = "#FFA000";
+  const ORANGE = "#f57200";
+  const INDIGO = "#303F9F";
+  const BLUE = "#0288D1";
+  const TEAL = "#00796B";
+  const PINK = "#C2185B";
+  const PURPLE = "#630b8c";
+  return (
+    <div id="btns-bar">
+      <Column />
+    </div>
+  );
+}
+export default BtnsBarSmall;
